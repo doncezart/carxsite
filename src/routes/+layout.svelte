3c75bd4023fd7e4b9036fa01a107c3cafe279037
Don Cezar <54808201+doncezart@users.noreply.github.com>
2024-01-25 00:08:34 +0200

    Analytics support via UMAMI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
     import Footer from '$lib/components/Footer.svelte'
 </script>
 
+<svelte:head>
+    <script defer src="https://eu.umami.is/script.js" data-website-id="fdcfad69-c717-43b7-81e7-8fd6db41b7e5"></script>
+</svelte:head>
+
 
 <div class="disclaimer">We are not directly affiliated with CarX Technologies or their products. This website is still under development with a lot of features missing.</div>
 <NavBar />
